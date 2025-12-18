d26ee9484ecb240669ffdce55510bc5f10e7b096
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2025-12-18 23:56:47 +0100

    (feat) Collects files to groups, adds typing

--- conv.py
+++ conv.py
@@ -2,6 +2,7 @@ import argparse
 import logging
 import subprocess
 from pathlib import Path
+from typing import TypedDict
 
 from dotenv import dotenv_values
 
@@ -10,11 +11,35 @@ logging.basicConfig(level=logging.INFO)
 __logger__ = logging.getLogger("converter")
 
 
+class FileInfo(TypedDict):
+    count: int
+    audio_bitrate: str | None
+    original_files: list[str]
+    new_files: list[str]
+
+
 def sanitize_file_name(filename: str) -> str:
     parts = filename.split('-')
     path_parts = _get_new_path_parts(parts)
     stem = path_parts.removesuffix(".mp4").removesuffix(" .mp4")
+    stem = _trim_index_if_exists(stem)
     return stem
+
+
+def _get_new_path_parts(parts: list) -> str:
+    if len(parts) < 6:
+        return Path("".join(parts).strip()).stem
+    return Path("".join(parts[6:]).strip()).stem
+
+
+def _trim_index_if_exists(stem: str) -> str:
+    suffix = stem[-3:]
+    try:
+        int(suffix)
+    except ValueError:
+        __logger__.info(f"suffix {suffix} is not int for {stem}")
+        return stem
+    return stem[:-3]
 
 
 def get_new_file_name(filename: str, index: int) -> str:
@@ -23,12 +48,6 @@ def get_new_file_name(filename: str, index: int) -> str:
     new_stem = path_parts.removesuffix(".mp4").removesuffix(" .mp4")
     new_fn = Path(f"{new_stem}-{index}").with_suffix(".mp4")
     return new_fn.name
-
-
-def _get_new_path_parts(parts: list) -> str:
-    if len(parts) < 6:
-        return Path("".join(parts).strip()).stem
-    return Path("".join(parts[6:]).strip()).stem
 
 
 def extract_audio_bitrate(filename: str):
@@ -54,12 +73,16 @@ def extract_audio_bitrate(filename: str):
     return bitrate
 
 
-def convert(sp: str, newdB: str, pattern: str, dry_run: bool):
-    audio_bitrate = None
-    for index, item in enumerate(Path(sp).glob(f"*{pattern}*.mp4")):
-        if not audio_bitrate:
-            audio_bitrate = extract_audio_bitrate(item.name)
-        new_file_name = get_new_file_name(item.name, index=index)
+def convert(file_map: dict[str, FileInfo], newdB: str, pattern: str, dry_run: bool):
+    # audio_bitrate = None
+    # for index, item in enumerate(Path(sp).glob(f"*{pattern}*.mp4")):
+        # if not audio_bitrate:
+            # audio_bitrate = extract_audio_bitrate(item.as_posix())
+        # new_file_name = get_new_file_name(item.name, index=index)
+    for video, video_data in file_map.item():
+        if  not video_data["audio_bitrate"]:
+            video_data["audio_bitrate"] = extract_audio_bitrate(item.as_posix())
+
         command = [
             "ffmpeg",
             "-i",
@@ -83,8 +106,8 @@ def convert(sp: str, newdB: str, pattern: str, dry_run: bool):
             __logger__.info(f"{item.name} -> {new_file_name} converted")
 
 
-def collect(folder: str, pattern: str) -> dict:
-    file_map = {}
+def collect(folder: str, pattern: str) -> dict[str, FileInfo]:
+    file_map: dict[str, FileInfo] = {}
     for item in Path(folder).glob(f"*{pattern}*.mp4"):
         stem = sanitize_file_name(item.name)
         if stem not in file_map:
@@ -95,7 +118,7 @@ def collect(folder: str, pattern: str) -> dict:
                 'original_files': [item.name],
                 'new_files': [Path(f"{stem}-{str(count)}").with_suffix(".mp4").as_posix()]
             }
-        if stem in file_map:
+        elif stem in file_map:
             count = file_map[stem].get("count") + 1
             file_map[stem]["count"] = count
             file_map[stem]['original_files'].append(item.name)
@@ -105,6 +128,9 @@ def collect(folder: str, pattern: str) -> dict:
 
 
 def get_args():
+    """
+    Getting the args
+    """
     parser = argparse.ArgumentParser(
         description="Batch adjust audio volume and re-encode MP4 files."
     )
@@ -128,8 +154,4 @@ if __name__ == '__main__':
         __logger__.error("Source path needs to be defined")
     run_args = get_args()
     fm = collect(folder=source_path, pattern=run_args.pattern)
-    print(fm)
-    for k, v in fm.items():
-        for x, y in zip(v["original_files"], v["new_files"]):
-            print(x, y)
-    convert(sp=source_path, newdB=run_args.dB, pattern=run_args.pattern, dry_run=run_args.dry_run)
+    convert(file_map=fm, newdB=run_args.dB, pattern=run_args.pattern, dry_run=run_args.dry_run)
